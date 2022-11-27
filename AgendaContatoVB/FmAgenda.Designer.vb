fc0b87f2d70f6ea9b3d4d1a860170c9eec3d3ad2
julyorybas <80602260+julyorybas@users.noreply.github.com>
2022-11-27 11:14:35 -0300

    first commit

---
+++ AgendaContatoVB/FmAgenda.Designer.vb
@@ -0,0 +1,217 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FmAgenda
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.LbId = New System.Windows.Forms.Label()
+        Me.LbNome = New System.Windows.Forms.Label()
+        Me.LbTelefone = New System.Windows.Forms.Label()
+        Me.Lbemail = New System.Windows.Forms.Label()
+        Me.TxId = New System.Windows.Forms.TextBox()
+        Me.TxNome = New System.Windows.Forms.TextBox()
+        Me.TxFone = New System.Windows.Forms.TextBox()
+        Me.TxEmail = New System.Windows.Forms.TextBox()
+        Me.BtSalvar = New System.Windows.Forms.Button()
+        Me.BtEditar = New System.Windows.Forms.Button()
+        Me.BtDeletar = New System.Windows.Forms.Button()
+        Me.BtConsultar = New System.Windows.Forms.Button()
+        Me.DgContatos = New System.Windows.Forms.DataGridView()
+        Me.BtLimpar = New System.Windows.Forms.Button()
+        CType(Me.DgContatos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'LbId
+        '
+        Me.LbId.AutoSize = True
+        Me.LbId.Location = New System.Drawing.Point(45, 9)
+        Me.LbId.Name = "LbId"
+        Me.LbId.Size = New System.Drawing.Size(21, 15)
+        Me.LbId.TabIndex = 0
+        Me.LbId.Text = "ID:"
+        '
+        'LbNome
+        '
+        Me.LbNome.AutoSize = True
+        Me.LbNome.Location = New System.Drawing.Point(23, 38)
+        Me.LbNome.Name = "LbNome"
+        Me.LbNome.Size = New System.Drawing.Size(43, 15)
+        Me.LbNome.TabIndex = 1
+        Me.LbNome.Text = "Nome:"
+        '
+        'LbTelefone
+        '
+        Me.LbTelefone.AutoSize = True
+        Me.LbTelefone.Location = New System.Drawing.Point(12, 67)
+        Me.LbTelefone.Name = "LbTelefone"
+        Me.LbTelefone.Size = New System.Drawing.Size(54, 15)
+        Me.LbTelefone.TabIndex = 2
+        Me.LbTelefone.Text = "Telefone:"
+        '
+        'Lbemail
+        '
+        Me.Lbemail.AutoSize = True
+        Me.Lbemail.Location = New System.Drawing.Point(211, 67)
+        Me.Lbemail.Name = "Lbemail"
+        Me.Lbemail.Size = New System.Drawing.Size(44, 15)
+        Me.Lbemail.TabIndex = 3
+        Me.Lbemail.Text = "E-mail:"
+        '
+        'TxId
+        '
+        Me.TxId.Location = New System.Drawing.Point(72, 6)
+        Me.TxId.Name = "TxId"
+        Me.TxId.Size = New System.Drawing.Size(62, 23)
+        Me.TxId.TabIndex = 8
+        '
+        'TxNome
+        '
+        Me.TxNome.Location = New System.Drawing.Point(72, 35)
+        Me.TxNome.Name = "TxNome"
+        Me.TxNome.Size = New System.Drawing.Size(454, 23)
+        Me.TxNome.TabIndex = 5
+        '
+        'TxFone
+        '
+        Me.TxFone.Location = New System.Drawing.Point(72, 64)
+        Me.TxFone.Name = "TxFone"
+        Me.TxFone.Size = New System.Drawing.Size(130, 23)
+        Me.TxFone.TabIndex = 6
+        '
+        'TxEmail
+        '
+        Me.TxEmail.Location = New System.Drawing.Point(261, 64)
+        Me.TxEmail.Name = "TxEmail"
+        Me.TxEmail.Size = New System.Drawing.Size(265, 23)
+        Me.TxEmail.TabIndex = 7
+        '
+        'BtSalvar
+        '
+        Me.BtSalvar.Location = New System.Drawing.Point(12, 278)
+        Me.BtSalvar.Name = "BtSalvar"
+        Me.BtSalvar.Size = New System.Drawing.Size(101, 23)
+        Me.BtSalvar.TabIndex = 8
+        Me.BtSalvar.Text = "Salvar"
+        Me.BtSalvar.UseVisualStyleBackColor = True
+        '
+        'BtEditar
+        '
+        Me.BtEditar.Enabled = False
+        Me.BtEditar.Location = New System.Drawing.Point(119, 278)
+        Me.BtEditar.Name = "BtEditar"
+        Me.BtEditar.Size = New System.Drawing.Size(101, 23)
+        Me.BtEditar.TabIndex = 9
+        Me.BtEditar.Text = "Editar"
+        Me.BtEditar.UseVisualStyleBackColor = True
+        '
+        'BtDeletar
+        '
+        Me.BtDeletar.Enabled = False
+        Me.BtDeletar.Location = New System.Drawing.Point(226, 278)
+        Me.BtDeletar.Name = "BtDeletar"
+        Me.BtDeletar.Size = New System.Drawing.Size(101, 23)
+        Me.BtDeletar.TabIndex = 10
+        Me.BtDeletar.Text = "Deletar"
+        Me.BtDeletar.UseVisualStyleBackColor = True
+        '
+        'BtConsultar
+        '
+        Me.BtConsultar.Location = New System.Drawing.Point(333, 278)
+        Me.BtConsultar.Name = "BtConsultar"
+        Me.BtConsultar.Size = New System.Drawing.Size(101, 23)
+        Me.BtConsultar.TabIndex = 11
+        Me.BtConsultar.Text = "Consultar"
+        Me.BtConsultar.UseVisualStyleBackColor = True
+        '
+        'DgContatos
+        '
+        Me.DgContatos.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells
+        Me.DgContatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DgContatos.Location = New System.Drawing.Point(12, 93)
+        Me.DgContatos.MultiSelect = False
+        Me.DgContatos.Name = "DgContatos"
+        Me.DgContatos.ReadOnly = True
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        Me.DgContatos.RowHeadersDefaultCellStyle = DataGridViewCellStyle1
+        Me.DgContatos.RowTemplate.Height = 25
+        Me.DgContatos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DgContatos.Size = New System.Drawing.Size(555, 179)
+        Me.DgContatos.TabIndex = 12
+        '
+        'BtLimpar
+        '
+        Me.BtLimpar.Location = New System.Drawing.Point(440, 278)
+        Me.BtLimpar.Name = "BtLimpar"
+        Me.BtLimpar.Size = New System.Drawing.Size(101, 23)
+        Me.BtLimpar.TabIndex = 13
+        Me.BtLimpar.Text = "Limpar Campos"
+        Me.BtLimpar.UseVisualStyleBackColor = True
+        '
+        'FmAgenda
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(593, 318)
+        Me.Controls.Add(Me.BtLimpar)
+        Me.Controls.Add(Me.DgContatos)
+        Me.Controls.Add(Me.BtConsultar)
+        Me.Controls.Add(Me.BtDeletar)
+        Me.Controls.Add(Me.BtEditar)
+        Me.Controls.Add(Me.BtSalvar)
+        Me.Controls.Add(Me.TxEmail)
+        Me.Controls.Add(Me.TxFone)
+        Me.Controls.Add(Me.TxNome)
+        Me.Controls.Add(Me.TxId)
+        Me.Controls.Add(Me.Lbemail)
+        Me.Controls.Add(Me.LbTelefone)
+        Me.Controls.Add(Me.LbNome)
+        Me.Controls.Add(Me.LbId)
+        Me.MaximizeBox = False
+        Me.Name = "FmAgenda"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Agenda Contato"
+        CType(Me.DgContatos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LbId As Label
+    Friend WithEvents LbNome As Label
+    Friend WithEvents LbTelefone As Label
+    Friend WithEvents Lbemail As Label
+    Friend WithEvents TxId As TextBox
+    Friend WithEvents TxNome As TextBox
+    Friend WithEvents TxFone As TextBox
+    Friend WithEvents TxEmail As TextBox
+    Friend WithEvents BtSalvar As Button
+    Friend WithEvents BtEditar As Button
+    Friend WithEvents BtDeletar As Button
+    Friend WithEvents BtConsultar As Button
+    Friend WithEvents DgContatos As DataGridView
+    Friend WithEvents BtLimpar As Button
+End Class
---
+++ AgendaContatoVB/FmAgenda.vb
@@ -0,0 +1,415 @@
+﻿Imports System.Data.SqlClient
+Imports System.Security.Policy
+
+
+Public Class FmAgenda
+
+    Private conexao As SqlConnection = New SqlConnection("Server=DESKTOP-TSST6LS\SQLEXPRESS; Database=AgendaContatoVB; User Id=sa; Password=abc123;")
+    Private comando As SqlCommand
+    Private dtAp As SqlDataAdapter
+    Private dtRead As SqlDataReader
+    Private strSQL As String
+
+    Private Fone As Integer
+
+    Private Sub FmAgenda_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        CarregarGrid()
+
+    End Sub
+
+    Private Sub BtSalvar_Click(sender As Object, e As EventArgs) Handles BtSalvar.Click
+
+        If (TxId.Text = "" And TxNome.Text <> "" And TxFone.Text <> "" And TxEmail.Text <> "") Then
+
+            Try
+                'conexao = New SqlConnection("Server=DESKTOP-TSST6LS\SQLEXPRESS; Database=AgendaContatoVB; User Id=sa; Password=abc123;")
+
+                strSQL = "INSERT INTO CONTATOS (NOME, TELEFONE, EMAIL) VALUES (@NOME, @TELEFONE, @EMAIL) "
+                comando = New SqlCommand(strSQL, conexao)
+                comando.Parameters.AddWithValue("@NOME", TxNome.Text)
+                comando.Parameters.AddWithValue("@TELEFONE", TxFone.Text)
+                comando.Parameters.AddWithValue("@EMAIL", TxEmail.Text)
+                conexao.Open()
+                comando.ExecuteNonQuery()
+
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+            Finally
+                conexao.Close()
+                comando.Clone()
+                comando = Nothing
+
+                MessageBox.Show("Contato cadastrado com sucesso", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+                CarregarGrid()
+                LimparCampos()
+
+            End Try
+
+
+        Else
+            MessageBox.Show("Favor preencher todos os campos", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+
+
+    End Sub
+
+
+    Private Sub CarregarGrid()
+        Try
+            'conexao = New SqlConnection("Server=DESKTOP-TSST6LS\SQLEXPRESS; Database=AgendaContatoVB; User Id=sa; Password=abc123;")
+            Dim ds As New DataSet
+            DgContatos.DataSource = Nothing
+
+            strSQL = "SELECT * FROM CONTATOS"
+            comando = New SqlCommand(strSQL, conexao)
+            dtAp = New SqlDataAdapter(strSQL, conexao)
+            conexao.Open()
+
+            dtAp.Fill(ds)
+            DgContatos.DataSource = ds.Tables(0)
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conexao.Close()
+            comando.Clone()
+            comando = Nothing
+        End Try
+
+    End Sub
+
+    Private Sub BtConsultar_Click(sender As Object, e As EventArgs) Handles BtConsultar.Click
+        'Try
+        '    'conexao = New SqlConnection("Server=DESKTOP-TSST6LS\SQLEXPRESS; Database=AgendaContatoVB; User Id=sa; Password=abc123;")
+
+        '    strSQL = "SELECT * FROM CONTATOS WHERE ID = @ID"
+        '    comando = New SqlCommand(strSQL, conexao)
+        '    comando.Parameters.AddWithValue("@ID", TxId.Text)
+        '    conexao.Open()
+
+        '    dtRead = comando.ExecuteReader
+
+        '    Do While dtRead.Read
+        '        TxNome.Text = dtRead("nome")
+        '        TxFone.Text = dtRead("telefone")
+        '        TxEmail.Text = dtRead("email")
+        '    Loop
+
+        'Catch ex As Exception
+        '    MessageBox.Show(ex.Message)
+        'Finally
+        '    conexao.Close()
+        '    comando.Clone()
+        '    comando = Nothing
+
+        'End Try
+
+        If (TxId.Text <> "") Then
+
+            ConsultarPorId()
+
+        ElseIf (TxNome.Text <> "") Then
+
+            ConsultarPorNome()
+
+        End If
+    End Sub
+
+    Private Sub BtEditar_Click(sender As Object, e As EventArgs) Handles BtEditar.Click
+
+        If (TxId.Text <> "") Then
+
+            BtSalvar.Enabled = False
+            BtEditar.Enabled = True
+
+            Try
+
+                strSQL = "UPDATE CONTATOS SET NOME = @NOME, TELEFONE = @TELEFONE, EMAIL = @EMAIL WHERE ID = @ID"
+
+                comando = New SqlCommand(strSQL, conexao)
+                comando.Parameters.AddWithValue("@ID", TxId.Text)
+                comando.Parameters.AddWithValue("@NOME", TxNome.Text)
+                comando.Parameters.AddWithValue("@TELEFONE", TxFone.Text)
+                comando.Parameters.AddWithValue("@EMAIL", TxEmail.Text)
+                conexao.Open()
+                comando.ExecuteNonQuery()
+
+            Catch ex As Exception
+
+                MessageBox.Show(ex.Message)
+
+            Finally
+
+                conexao.Close()
+                comando.Clone()
+                comando = Nothing
+
+                MessageBox.Show("Contato Alterado com sucesso", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+                CarregarGrid()
+                LimparCampos()
+
+            End Try
+
+        Else
+            BtEditar.Enabled = False
+        End If
+    End Sub
+
+    Private Sub BtDeletar_Click(sender As Object, e As EventArgs) Handles BtDeletar.Click
+
+        If (TxId.Text <> "") Then
+
+            BtSalvar.Enabled = False
+            BtDeletar.Enabled = True
+
+            Try
+
+                strSQL = "DELETE CONTATOS WHERE ID = @ID"
+
+                comando = New SqlCommand(strSQL, conexao)
+                comando.Parameters.AddWithValue("@ID", TxId.Text)
+                conexao.Open()
+                comando.ExecuteNonQuery()
+
+            Catch ex As Exception
+
+                MessageBox.Show(ex.Message)
+
+            Finally
+
+                conexao.Close()
+                comando.Clone()
+                comando = Nothing
+
+                MessageBox.Show("Contato Excluido com sucesso", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+                CarregarGrid()
+                LimparCampos()
+
+            End Try
+
+        Else
+
+            BtDeletar.Enabled = False
+
+        End If
+    End Sub
+
+    Private Sub ConsultarPorId()
+
+        Try
+            'conexao = New SqlConnection("Server=DESKTOP-TSST6LS\SQLEXPRESS; Database=AgendaContatoVB; User Id=sa; Password=abc123;")
+
+            strSQL = "SELECT * FROM CONTATOS WHERE ID = @ID"
+            comando = New SqlCommand(strSQL, conexao)
+            comando.Parameters.AddWithValue("@ID", TxId.Text)
+            conexao.Open()
+
+            dtRead = comando.ExecuteReader
+
+            Do While dtRead.Read
+                TxNome.Text = dtRead("nome")
+                TxFone.Text = dtRead("telefone")
+                TxEmail.Text = dtRead("email")
+            Loop
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+
+            conexao.Close()
+            comando.Clone()
+            comando = Nothing
+            dtRead = Nothing
+
+            CarregarGridConsultaPorId()
+
+        End Try
+
+    End Sub
+
+    Private Sub CarregarGridConsultaPorId()
+        Try
+            'conexao = New SqlConnection("Server=DESKTOP-TSST6LS\SQLEXPRESS; Database=AgendaContatoVB; User Id=sa; Password=abc123;")
+            Dim ds As New DataSet
+            DgContatos.DataSource = Nothing
+
+            strSQL = "SELECT * FROM CONTATOS WHERE ID = " & TxId.Text & ""
+            comando = New SqlCommand(strSQL, conexao)
+            dtAp = New SqlDataAdapter(strSQL, conexao)
+            conexao.Open()
+
+            dtAp.Fill(ds)
+            DgContatos.DataSource = ds.Tables(0)
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conexao.Close()
+            comando.Clone()
+            comando = Nothing
+        End Try
+
+    End Sub
+
+
+    Private Sub ConsultarPorNome()
+
+        Try
+            'conexao = New SqlConnection("Server=DESKTOP-TSST6LS\SQLEXPRESS; Database=AgendaContatoVB; User Id=sa; Password=abc123;")
+
+            strSQL = "SELECT * FROM CONTATOS WHERE NOME LIKE '%@NOME%'"
+            comando = New SqlCommand(strSQL, conexao)
+            comando.Parameters.AddWithValue("@NOME", TxNome.Text)
+            conexao.Open()
+
+            dtRead = comando.ExecuteReader
+
+            Do While dtRead.Read
+                TxNome.Text = dtRead("nome")
+                TxFone.Text = dtRead("telefone")
+                TxEmail.Text = dtRead("email")
+            Loop
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+
+            conexao.Close()
+            comando.Clone()
+            comando = Nothing
+            dtRead = Nothing
+
+            CarregarGridConsultaPorNome()
+
+        End Try
+
+    End Sub
+
+    Private Sub CarregarGridConsultaPorNome()
+        Try
+            'conexao = New SqlConnection("Server=DESKTOP-TSST6LS\SQLEXPRESS; Database=AgendaContatoVB; User Id=sa; Password=abc123;")
+            Dim ds As New DataSet
+            DgContatos.DataSource = Nothing
+
+            strSQL = "SELECT * FROM CONTATOS WHERE NOME LIKE '%" & TxNome.Text & "%'"
+            comando = New SqlCommand(strSQL, conexao)
+            dtAp = New SqlDataAdapter(strSQL, conexao)
+            conexao.Open()
+
+            dtAp.Fill(ds)
+            DgContatos.DataSource = ds.Tables(0)
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conexao.Close()
+            comando.Clone()
+            comando = Nothing
+        End Try
+
+    End Sub
+
+    Private Sub LimparCampos()
+
+        TxId.Text = ""
+        TxNome.Text = ""
+        TxFone.Text = ""
+        TxEmail.Text = ""
+
+        BtEditar.Enabled = False
+        BtDeletar.Enabled = False
+        BtSalvar.Enabled = True
+
+    End Sub
+
+    Private Sub TxId_Leave(sender As Object, e As EventArgs) Handles TxId.Leave
+        If (TxId.Text <> "") Then
+
+            BtSalvar.Enabled = False
+            BtEditar.Enabled = True
+            BtDeletar.Enabled = True
+            ConsultarPorId()
+
+        End If
+    End Sub
+
+    Private Sub TxFone_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TxFone.KeyPress
+        If Not (Char.IsDigit(e.KeyChar) OrElse Char.IsControl(e.KeyChar)) Then
+
+            e.Handled = True
+
+        End If
+    End Sub
+
+    Private Sub BtLimpar_Click(sender As Object, e As EventArgs) Handles BtLimpar.Click
+        LimparCampos()
+        CarregarGrid()
+    End Sub
+
+    'Private Sub DgContatos_CellContentDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles DgContatos.CellContentDoubleClick
+    '    If (DgContatos.RowCount > 0) Then
+
+    '        TxId.Text = DgContatos.CurrentRow.Cells(0).Value
+    '        TxNome.Text = DgContatos.CurrentRow.Cells(1).Value
+    '        TxFone.Text = DgContatos.CurrentRow.Cells(2).Value
+    '        TxEmail.Text = DgContatos.CurrentRow.Cells(3).Value
+
+    '    End If
+    'End Sub
+
+    Private Sub DgContatos_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DgContatos.CellContentClick
+        If (DgContatos.RowCount > 0) Then
+
+            TxId.Text = DgContatos.CurrentRow.Cells(0).Value
+            TxNome.Text = DgContatos.CurrentRow.Cells(1).Value
+            TxFone.Text = DgContatos.CurrentRow.Cells(2).Value
+            TxEmail.Text = DgContatos.CurrentRow.Cells(3).Value
+
+            BtSalvar.Enabled = False
+            BtEditar.Enabled = True
+            BtDeletar.Enabled = True
+
+        End If
+    End Sub
+
+    Private Sub TxId_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TxId.KeyPress
+        If Not (Char.IsDigit(e.KeyChar) OrElse Char.IsControl(e.KeyChar)) Then
+
+            e.Handled = True
+
+        End If
+    End Sub
+
+
+
+
+
+    'Private Sub TxFone_Leave(sender As Object, e As EventArgs) Handles TxFone.Leave
+    '    If (TxFone.Text <> "") Then
+
+
+    '        'TxFone.Text = Format$(TxFone.Text, "!(@@) @@@-@@@@")
+    '        'verifica valores invalidos
+    '        For i = Len(TxFone.Text) To 1 Step -1
+    '            If InStr("0123456789", Mid$(TxFone.Text, i, 1)) = 0 Then
+    '                TxFone.Text = Left$(TxFone.Text, i - 1) & Mid$(TxFone.Text, i + 1)
+    '            End If
+    '        Next
+    '        ' ajusta a posicao correta
+    '        If Len(Fone) <= 7 Then
+    '            TxFone.Text = Format$(TxFone.Text, "!@@@-@@@@")
+    '        ElseIf Len(Fone) > 7 And Len(TxFone.Text) <= 9 Then
+    '            TxFone.Text = Format$(TxFone.Text, "!(@@) @@@-@@@@")
+    '        ElseIf Len(Fone) > 9 Then
+    '            TxFone.Text = Format$(TxFone.Text, "!(@@) @@@@-@@@@")
+    '        End If
+
+    '    End If
+    'End Sub
+
+
+End Class
